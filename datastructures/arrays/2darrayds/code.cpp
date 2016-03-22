0ee883bc8088c6c6ff03b3b6bc0bdf538e04afbc
wguo <wei.guo@tekcomms.com>
2016-03-22 15:07:55 +0800

    Implement 2 Array cases.

---
+++ datastructures/arrays/2darrayds/code.cpp
@@ -0,0 +1,48 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    
+    //int sum = arr[0][0]+arr[0][1]+arr[0][2]+arr[1][1]+arr[2][0]+arr[2][1]+arr[2][2];
+    set<int> sum;
+    for(int i=1;i<5;++i){
+        for(int j=1;j<5;++j){
+            //int tmp = arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]+arr[i][j]+arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1];
+            //if(tmp>sum){
+            //    sum = tmp;
+            //}
+            sum.insert(arr[i-1][j-1]+arr[i-1][j]+arr[i-1][j+1]+arr[i][j]+arr[i+1][j-1]+arr[i+1][j]+arr[i+1][j+1]);
+        }
+    }
+    cout<<*(sum.rbegin())<<endl;
+    return 0;
+}
