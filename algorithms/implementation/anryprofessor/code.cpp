e067fbe3b0d6d0bf12fbddd0189a56dae3d6a29c
wguo <wei.guo@tekcomms.com>
2016-03-30 15:48:13 +0800

    Update.

---
+++ algorithms/implementation/anryprofessor/code.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        int k;
+        int num = 0;
+        cin >> n >> k;
+        vector<int> a(n);
+        for(int a_i = 0;a_i < n;a_i++){
+           cin >> a[a_i];
+           if(a[a_i]<=0){
+               ++num;
+           }
+        }
+        cout<<(num<k?"YES":"NO")<<endl;
+    }
+    return 0;
+}
